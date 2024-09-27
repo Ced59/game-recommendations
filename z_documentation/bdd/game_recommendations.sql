814159a6889dc78e58393cb4a375257616fecb7e
Ced59 <c.caudron59@gmail.com>
2024-09-27 23:18:05 +0200

    Add UNIQUE index to pseudo in user table

--- z_documentation/bdd/game_recommendations.sql
+++ z_documentation/bdd/game_recommendations.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 27 sep. 2024 à 16:43
+-- Généré le : ven. 27 sep. 2024 à 21:16
 -- Version du serveur : 8.3.0
 -- Version de PHP : 8.2.18
 
@@ -38,7 +38,15 @@ CREATE TABLE IF NOT EXISTS `games` (
   `genre` varchar(50) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `games`
+--
+
+INSERT INTO `games` (`id`, `title`, `developer`, `release_year`, `genre`, `description`) VALUES
+(1, 'The Legend Of Zelda : Echoes Of Wisdom', 'Nintendo', '2024', 'Action/Aventure', 'Dans \"The Legend of Zelda: Echoes of Wisdom\", vous incarnez la princesse Zelda elle-même dans une aventure passionnante pour sauver Hyrule. De mystérieuses failles sont apparues, engloutissant les habitants, y compris Link. Accompagnée de la fée Tri, Zelda devra utiliser sa sagesse et ses nouveaux pouvoirs pour résoudre les énigmes, explorer des donjons et combattre des ennemis.'),
+(2, 'The Legend Of Zelda : Ocarina of Time', 'Nintendo', '1998', 'Action/Aventure', '\"The Legend of Zelda: Ocarina of Time\" est un jeu d\'action-aventure emblématique sorti en 1998, considéré comme l\'un des plus grands jeux vidéo de tous les temps. Vous incarnez Link, un jeune garçon Kokiri, qui se lance dans une quête épique à travers le temps pour sauver Hyrule du maléfique Ganondorf.');
 
 -- --------------------------------------------------------
 
@@ -51,7 +59,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_pseudo` (`pseudo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
