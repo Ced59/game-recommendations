a5e17f6b78358c64cdf121b354d87aae94f92986
Ced59 <c.caudron59@gmail.com>
2024-09-27 18:54:00 +0200

    Routing HomeController Works!

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -3,5 +3,8 @@
 namespace Controllers;
 
 class HomeController {
-    public function index() {}
+    public function index(): void
+    {
+        require_once __DIR__ . '/../views/home/index.php'; // TODO Ne comprends pas pour le moment pourquoi le chemin relatif n'est possible qu'avec _DIR_... Sûrement à cause de Wamp? Investigation requise.
+    }
 }
---
+++ views/home/index.php
@@ -0,0 +1,1 @@
+<h1>Test routing</h1>
