74cec6c8aec5cf770e445664ba01e321d7c83c24
Ced59 <c.caudron59@gmail.com>
2024-09-27 21:41:18 +0200

    Display list of games OK

--- views/games/index.php
+++ views/games/index.php
@@ -3,3 +3,11 @@ echo "Page jeu fonctionnelle";
 ?>
 
 <h1>Page jeux fonctionnelle</h1>
+
+<?php foreach ($games as $game): ?>
+    <h3><?= $game['title'] ?></h3>
+    <p>Développeur: <?= $game['developer']?></p>
+    <p>Genre: <?= $game['genre']?></p>
+    <p>Description: <?= $game['description']?></p>
+    <p>Année de sortie: <?= $game['release_year']?></p>
+<?php endforeach; ?>
