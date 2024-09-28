f739f1a4864d5c3ef21e40b1ee280ee8ff2d1862
Ced59 <c.caudron59@gmail.com>
2024-09-28 17:25:05 +0200

    Rewrite navbar with bootstrap 5.3

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,12 +1,15 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <div class="container-fluid">
         <a class="navbar-brand" href="/GameRating/">GameRating</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav ms-auto d-flex align-items-center">
                 <?php if (isset($_SESSION['pseudo'])): ?>
                     <li class="nav-item">
-                        <span class="navbar-text">Vous êtes connecté en tant que <?= htmlspecialchars($_SESSION['pseudo'], ENT_QUOTES, 'UTF-8') ?></span>
+                        <span class="navbar-text me-3">Vous êtes connecté en tant que <?= htmlspecialchars($_SESSION['pseudo'], ENT_QUOTES, 'UTF-8') ?></span>
                     </li>
                     <li class="nav-item">
                         <form action="/GameRating/index.php" method="post" class="d-inline">
