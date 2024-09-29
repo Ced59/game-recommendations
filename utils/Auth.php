517bc2c5af83d267ec08783a0caa6e2c201ed003
Ced59 <c.caudron59@gmail.com>
2024-09-29 09:36:55 +0200

    Add error message when user want to go on a protected page without authentication

--- utils/Auth.php
+++ utils/Auth.php
@@ -6,10 +6,13 @@ class Auth {
     public static function checkAuth(): void
     {
         if (!isset($_SESSION['user_id'])) {
+
+            $_SESSION['error'] = "Vous devez être authentifié pour accéder à la page demandée.";
+
             header('Location: /GameRating/index.php');
             exit();
 
-            //TODO: à améliorer => au moins faire afficher un message comme quoi il faut être authentifié pour accéder à la page, au mieux une redirection vers la page d'origine!
+            //TODO: à améliorer => avec une redirection vers la page d'origine!
         }
     }
 }
